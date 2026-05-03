3dee2bb2af943e0e7159ea3f7f6827cf6e3300fd
shohei ohara <ivis.klain@chromatri.be>
2026-05-04 02:19:14 +0900

    chore: バージョン 1.20260504.1 へのアップデートとローカライズ微調整

--- Qlypx/Generated/LocalizedStrings.swift
+++ Qlypx/Generated/LocalizedStrings.swift
@@ -11,161 +11,161 @@ import Foundation
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
   /// Add
-  internal static var add: String { L10n.tr("Localizable", "Add")}
+  internal static var add: String { L10n.tr("Localizable", "Add") }
   /// Are you sure want to delete this item?
-  internal static var areYouSureWantToDeleteThisItem: String { L10n.tr("Localizable", "Are you sure want to delete this item?")}
+  internal static var areYouSureWantToDeleteThisItem: String { L10n.tr("Localizable", "Are you sure want to delete this item?") }
   /// Are you sure you want to clear your clipboard history?
-  internal static var areYouSureYouWantToClearYourClipboardHistory: String { L10n.tr("Localizable", "Are you sure you want to clear your clipboard history?")}
+  internal static var areYouSureYouWantToClearYourClipboardHistory: String { L10n.tr("Localizable", "Are you sure you want to clear your clipboard history?") }
   /// Cancel
-  internal static var cancel: String { L10n.tr("Localizable", "Cancel")}
+  internal static var cancel: String { L10n.tr("Localizable", "Cancel") }
   /// Clear History
-  internal static var clearHistory: String { L10n.tr("Localizable", "Clear History")}
+  internal static var clearHistory: String { L10n.tr("Localizable", "Clear History") }
   /// Delete Item
-  internal static var deleteItem: String { L10n.tr("Localizable", "Delete Item")}
+  internal static var deleteItem: String { L10n.tr("Localizable", "Delete Item") }
   /// Don't Launch
-  internal static var donTLaunch: String { L10n.tr("Localizable", "Don't Launch")}
+  internal static var donTLaunch: String { L10n.tr("Localizable", "Don't Launch") }
   /// Edit Snippets...
-  internal static var editSnippets: String { L10n.tr("Localizable", "Edit Snippets")}
+  internal static var editSnippets: String { L10n.tr("Localizable", "Edit Snippets") }
   /// General
-  internal static var general: String { L10n.tr("Localizable", "General")}
+  internal static var general: String { L10n.tr("Localizable", "General") }
   /// History
-  internal static var history: String { L10n.tr("Localizable", "History")}
+  internal static var history: String { L10n.tr("Localizable", "History") }
   /// Launch Qlypx on system startup?
-  internal static var launchQlypxOnSystemStartup: String { L10n.tr("Localizable", "Launch Qlypx on system startup?")}
+  internal static var launchQlypxOnSystemStartup: String { L10n.tr("Localizable", "Launch Qlypx on system startup?") }
   /// Launch on system startup
-  internal static var launchOnSystemStartup: String { L10n.tr("Localizable", "Launch on system startup")}
+  internal static var launchOnSystemStartup: String { L10n.tr("Localizable", "Launch on system startup") }
   /// Menu
-  internal static var menu: String { L10n.tr("Localizable", "Menu")}
+  internal static var menu: String { L10n.tr("Localizable", "Menu") }
   /// Open System Preferences
-  internal static var openSystemPreferences: String { L10n.tr("Localizable", "Open System Preferences")}
+  internal static var openSystemPreferences: String { L10n.tr("Localizable", "Open System Preferences") }
   /// Please allow Accessibility.
-  internal static var pleaseAllowAccessibility: String { L10n.tr("Localizable", "Please allow Accessibility")}
+  internal static var pleaseAllowAccessibility: String { L10n.tr("Localizable", "Please allow Accessibility") }
   /// Please fill in the contents of the snippet
-  internal static var pleaseFillInTheContentsOfTheSnippet: String { L10n.tr("Localizable", "Please fill in the contents of the snippet")}
+  internal static var pleaseFillInTheContentsOfTheSnippet: String { L10n.tr("Localizable", "Please fill in the contents of the snippet") }
   /// Preferences...
-  internal static var preferences: String { L10n.tr("Localizable", "Preferences")}
+  internal static var preferences: String { L10n.tr("Localizable", "Preferences") }
   /// Quit Qlypx
-  internal static var quitQlypx: String { L10n.tr("Localizable", "Quit Qlypx")}
+  internal static var quitQlypx: String { L10n.tr("Localizable", "Quit Qlypx") }
   /// Shortcuts
-  internal static var shortcuts: String { L10n.tr("Localizable", "Shortcuts")}
+  internal static var shortcuts: String { L10n.tr("Localizable", "Shortcuts") }
   /// Snippet
-  internal static var snippet: String { L10n.tr("Localizable", "Snippet")}
-  /// To do this action please allow Accessibility in Security & Privacy preferences, located in System Preferences.
-  internal static var toDoThisActionPleaseAllowAccessibilityInSecurityPrivacyPreferencesLocatedInSystemPreferences: String { L10n.tr("Localizable", "To do this action please allow Accessibility in Security Privacy preferences located in System Preferences")}
+  internal static var snippet: String { L10n.tr("Localizable", "Snippet") }
+  /// To do this action please allow Accessibility in Security Privacy preferences located in System Preferences.
+  internal static var toDoThisActionPleaseAllowAccessibilityInSecurityPrivacyPreferencesLocatedInSystemPreferences: String { L10n.tr("Localizable", "To do this action please allow Accessibility in Security Privacy preferences located in System Preferences") }
   /// Type
-  internal static var type: String { L10n.tr("Localizable", "Type")}
+  internal static var type: String { L10n.tr("Localizable", "Type") }
   /// Updates
-  internal static var updates: String { L10n.tr("Localizable", "Updates")}
+  internal static var updates: String { L10n.tr("Localizable", "Updates") }
   /// You can change this setting in the Preferences if you want.
-  internal static var youCanChangeThisSettingInThePreferencesIfYouWant: String { L10n.tr("Localizable", "You can change this setting in the Preferences if you want")}
+  internal static var youCanChangeThisSettingInThePreferencesIfYouWant: String { L10n.tr("Localizable", "You can change this setting in the Preferences if you want") }
   /// Add Folder
-  internal static var addFolder: String { L10n.tr("Localizable", "Add Folder")}
+  internal static var addFolder: String { L10n.tr("Localizable", "Add Folder") }
   /// Add Snippet
-  internal static var addSnippet: String { L10n.tr("Localizable", "Add Snippet")}
+  internal static var addSnippet: String { L10n.tr("Localizable", "Add Snippet") }
   /// Delete
-  internal static var delete: String { L10n.tr("Localizable", "Delete")}
+  internal static var delete: String { L10n.tr("Localizable", "Delete") }
   /// Enable/Disable
-  internal static var enableDisable: String { L10n.tr("Localizable", "Enable/Disable")}
+  internal static var enableDisable: String { L10n.tr("Localizable", "Enable/Disable") }
   /// Import CSV
-  internal static var importCsv: String { L10n.tr("Localizable", "Import CSV")}
+  internal static var importCsv: String { L10n.tr("Localizable", "Import CSV") }
   /// Export CSV
-  internal static var exportCsv: String { L10n.tr("Localizable", "Export CSV")}
+  internal static var exportCsv: String { L10n.tr("Localizable", "Export CSV") }
   /// Image
-  internal static var image: String { L10n.tr("Localizable", "Image")}
+  internal static var image: String { L10n.tr("Localizable", "Image") }
   /// PDF
-  internal static var pdf: String { L10n.tr("Localizable", "PDF")}
+  internal static var pdf: String { L10n.tr("Localizable", "PDF") }
   /// Filenames
-  internal static var filenames: String { L10n.tr("Localizable", "Filenames")}
+  internal static var filenames: String { L10n.tr("Localizable", "Filenames") }
   /// Exclude
-  internal static var exclude: String { L10n.tr("Localizable", "Exclude")}
+  internal static var exclude: String { L10n.tr("Localizable", "Exclude") }
   /// untitled folder
-  internal static var untitledFolder: String { L10n.tr("Localizable", "Untitled Folder")}
+  internal static var untitledFolder: String { L10n.tr("Localizable", "Untitled Folder") }
   /// untitled snippet
-  internal static var untitledSnippet: String { L10n.tr("Localizable", "Untitled Snippet")}
+  internal static var untitledSnippet: String { L10n.tr("Localizable", "Untitled Snippet") }
   /// Others
-  internal static var others: String { L10n.tr("Localizable", "Others")}
+  internal static var others: String { L10n.tr("Localizable", "Others") }
   /// Menu Items
-  internal static var menuItems: String { L10n.tr("Localizable", "Menu Items")}
+  internal static var menuItems: String { L10n.tr("Localizable", "Menu Items") }
   /// Appearance
-  internal static var appearance: String { L10n.tr("Localizable", "Appearance")}
+  internal static var appearance: String { L10n.tr("Localizable", "Appearance") }
   /// Solid
-  internal static var black: String { L10n.tr("Localizable", "Black")}
+  internal static var black: String { L10n.tr("Localizable", "Black") }
   /// App Icon
-  internal static var color: String { L10n.tr("Localizable", "Color")}
+  internal static var color: String { L10n.tr("Localizable", "Color") }
   /// Display icons in menu items
-  internal static var displayIconsInMenuItems: String { L10n.tr("Localizable", "Display icons in menu items")}
+  internal static var displayIconsInMenuItems: String { L10n.tr("Localizable", "Display icons in menu items") }
   /// Height
-  internal static var height: String { L10n.tr("Localizable", "Height")}
+  internal static var height: String { L10n.tr("Localizable", "Height") }
   /// Mark menu items with numbers
-  internal static var markMenuItemsWithNumbers: String { L10n.tr("Localizable", "Mark menu items with numbers")}
+  internal static var markMenuItemsWithNumbers: String { L10n.tr("Localizable", "Mark menu items with numbers") }
   /// Max characters in the menu
-  internal static var maxCharactersInTheMenu: String { L10n.tr("Localizable", "Max characters in the menu")}
+  internal static var maxCharactersInTheMenu: String { L10n.tr("Localizable", "Max characters in the menu") }
   /// Max length of tool tip
-  internal static var maxLengthOfToolTip: String { L10n.tr("Localizable", "Max length of tool tip")}
+  internal static var maxLengthOfToolTip: String { L10n.tr("Localizable", "Max length of tool tip") }
   /// Move to top
-  internal static var moveToTop: String { L10n.tr("Localizable", "Move to top")}
+  internal static var moveToTop: String { L10n.tr("Localizable", "Move to top") }
   /// None
-  internal static var none: String { L10n.tr("Localizable", "None")}
+  internal static var none: String { L10n.tr("Localizable", "None") }
   /// Number of items inside a folder
-  internal static var numberOfItemsInsideAFolder: String { L10n.tr("Localizable", "Number of items inside a folder")}
+  internal static var numberOfItemsInsideAFolder: String { L10n.tr("Localizable", "Number of items inside a folder") }
   /// Number of items place inline
-  internal static var numberOfItemsPlaceInline: String { L10n.tr("Localizable", "Number of items place inline")}
+  internal static var numberOfItemsPlaceInline: String { L10n.tr("Localizable", "Number of items place inline") }
   /// Order after pasting
-  internal static var orderAfterPasting: String { L10n.tr("Localizable", "Order after pasting")}
+  internal static var orderAfterPasting: String { L10n.tr("Localizable", "Order after pasting") }
   /// Remember history size
-  internal static var rememberHistorySize: String { L10n.tr("Localizable", "Remember history size")}
+  internal static var rememberHistorySize: String { L10n.tr("Localizable", "Remember history size") }
   /// Send crash reports & error logs
-  internal static var sendCrashReportsErrorLogs: String { L10n.tr("Localizable", "Send crash reports & error logs")}
+  internal static var sendCrashReportsErrorLogs: String { L10n.tr("Localizable", "Send crash reports & error logs") }
   /// Show color code preview
-  internal static var showColorCodePreview: String { L10n.tr("Localizable", "Show color code preview")}
+  internal static var showColorCodePreview: String { L10n.tr("Localizable", "Show color code preview") }
   /// Show Image
-  internal static var showImage: String { L10n.tr("Localizable", "Show Image")}
+  internal static var showImage: String { L10n.tr("Localizable", "Show Image") }
   /// Show tool tip on a menu item
-  internal static var showToolTipOnAMenuItem: String { L10n.tr("Localizable", "Show tool tip on a menu item")}
+  internal static var showToolTipOnAMenuItem: String { L10n.tr("Localizable", "Show tool tip on a menu item") }
   /// Status bar icon
-  internal static var statusBarIcon: String { L10n.tr("Localizable", "Status bar icon")}
+  internal static var statusBarIcon: String { L10n.tr("Localizable", "Status bar icon") }
   /// Line
-  internal static var white: String { L10n.tr("Localizable", "White")}
+  internal static var white: String { L10n.tr("Localizable", "White") }
   /// Width
-  internal static var width: String { L10n.tr("Localizable", "Width")}
+  internal static var width: String { L10n.tr("Localizable", "Width") }
   /// Select clipboard types to store:
-  internal static var selectClipboardTypesToStore: String { L10n.tr("Localizable", "Select clipboard types to store:")}
+  internal static var selectClipboardTypesToStore: String { L10n.tr("Localizable", "Select clipboard types to store:") }
   /// Exclude these applications:
-  internal static var excludeTheseApplications: String { L10n.tr("Localizable", "Exclude these applications:")}
+  internal static var excludeTheseApplications: String { L10n.tr("Localizable", "Exclude these applications:") }
   /// Clipboard Monitoring Speed
-  internal static var monitoringSpeed: String { L10n.tr("Localizable", "Monitoring Speed")}
-  /// High Speed (0.2s)}
-  internal static var highSpeed: String { L10n.tr("Localizable", "High Speed (0.2s)}")}
-  /// Fast (0.5s)}
-  internal static var fast: String { L10n.tr("Localizable", "Fast (0.5s)}")}
-  /// Standard (0.75s)}
-  internal static var standard: String { L10n.tr("Localizable", "Standard (0.75s)}")}
-  /// Power Saving (1.0s)}
-  internal static var powerSaving: String { L10n.tr("Localizable", "Power Saving (1.0s)}")}
+  internal static var monitoringSpeed: String { L10n.tr("Localizable", "Monitoring Speed") }
+  /// High Speed (0.2s)
+  internal static var highSpeed: String { L10n.tr("Localizable", "High Speed (0.2s)") }
+  /// Fast (0.5s)
+  internal static var fast: String { L10n.tr("Localizable", "Fast (0.5s)") }
+  /// Standard (0.75s)
+  internal static var standard: String { L10n.tr("Localizable", "Standard (0.75s)") }
+  /// Power Saving (1.0s)
+  internal static var powerSaving: String { L10n.tr("Localizable", "Power Saving (1.0s)") }
   /// Language
-  internal static var language: String { L10n.tr("Localizable", "Language")}
+  internal static var language: String { L10n.tr("Localizable", "Language") }
   /// System Language
-  internal static var systemLanguage: String { L10n.tr("Localizable", "System Language")}
+  internal static var systemLanguage: String { L10n.tr("Localizable", "System Language") }
   /// Japanese
-  internal static var japanese: String { L10n.tr("Localizable", "Japanese")}
+  internal static var japanese: String { L10n.tr("Localizable", "Japanese") }
   /// English
-  internal static var english: String { L10n.tr("Localizable", "English")}
+  internal static var english: String { L10n.tr("Localizable", "English") }
   /// German
-  internal static var german: String { L10n.tr("Localizable", "German")}
+  internal static var german: String { L10n.tr("Localizable", "German") }
   /// Italian
-  internal static var italian: String { L10n.tr("Localizable", "Italian")}
+  internal static var italian: String { L10n.tr("Localizable", "Italian") }
   /// Simplified Chinese
-  internal static var simplifiedChinese: String { L10n.tr("Localizable", "Simplified Chinese")}
+  internal static var simplifiedChinese: String { L10n.tr("Localizable", "Simplified Chinese") }
   /// Plain Text
-  internal static var plainText: String { L10n.tr("Localizable", "Plain Text")}
+  internal static var plainText: String { L10n.tr("Localizable", "Plain Text") }
   /// Rich Text Format (RTF)
-  internal static var rtf: String { L10n.tr("Localizable", "Rich Text Format (RTF)")}
+  internal static var rtf: String { L10n.tr("Localizable", "Rich Text Format (RTF)") }
   /// Rich Text Format Directory (RTFD)
-  internal static var rtfd: String { L10n.tr("Localizable", "Rich Text Format Directory (RTFD)")}
+  internal static var rtfd: String { L10n.tr("Localizable", "Rich Text Format Directory (RTFD)") }
   /// TIFF Image
-  internal static var tiffImage: String { L10n.tr("Localizable", "TIFF Image")}
+  internal static var tiffImage: String { L10n.tr("Localizable", "TIFF Image") }
   /// URL
-  internal static var url: String { L10n.tr("Localizable", "URL")}
+  internal static var url: String { L10n.tr("Localizable", "URL") }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
@@ -203,4 +203,3 @@ private final class BundleToken {
     return Bundle(for: BundleToken.self)
   }
 }
-// swiftlint:enable convenience_type
